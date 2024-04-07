8edc8242f78046c06de66804830c352290fc661b
vokadum <wasi786@mail.ru>
2024-04-07 14:29:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+    System.out.print("Amigo ");
+    System.out.print("The ");
+    System.out.print("Best");
+    }
+}
+
+
+
